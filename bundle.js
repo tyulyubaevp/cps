f9b132c4fbc559fa75b2d4462bc989f1d8f2fbff
tyulyubaevp <solistenebrum@gmail.com>
2025-07-04 17:26:11 +0700

    Updates

--- bundle.js
+++ bundle.js
@@ -27,6 +27,13 @@ sidebarOverlay.addEventListener('click', function () {
 /***/ (function() {
 
 const carouselButtons = document.querySelectorAll('.hero__carousel-button');
+const toggleReadMoreButton = document.querySelector('.hero__more-button');
+const toggleReadMoreButtonText = toggleReadMoreButton.querySelector('.hero__more-button-text');
+const toggleReadMoreButtonExpandIcon = toggleReadMoreButton.querySelector('.hero__expand-icon');
+const textSecond = document.querySelector('.hero__text_second');
+const textAdd = document.querySelector('.hero__add-text');
+const textThird = document.querySelector('.hero__text_third');
+let expanded = false;
 carouselButtons.forEach(btn => {
   btn.addEventListener('click', function () {
     const isActive = btn.classList.contains('hero__carousel-button_active');
@@ -35,6 +42,21 @@ carouselButtons.forEach(btn => {
       btn.classList.add('hero__carousel-button_active');
     }
   });
+});
+toggleReadMoreButton.addEventListener('click', function () {
+  expanded = !expanded;
+  toggleReadMoreButtonText.textContent = expanded ? 'Скрыть' : 'Читать далее';
+  toggleReadMoreButtonExpandIcon.classList.toggle('hero__expand-icon_active');
+  if (window.innerWidth >= 320 && window.innerWidth < 768) {
+    textSecond.classList.toggle('hero__text_visible');
+    textAdd.classList.toggle('hero__add-text_visible');
+    textThird.classList.toggle('hero__text_visible');
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1120) {
+    textAdd.classList.toggle('hero__add-text_visible');
+    textThird.classList.toggle('hero__text_visible');
+  } else {
+    textThird.classList.toggle('hero__text_visible');
+  }
 });
 
 /***/ }),
