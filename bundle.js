297bd35ed37e9c20ada9d873667dbe1d938fc45d
tyulyubaevp <solistenebrum@gmail.com>
2025-07-06 17:49:47 +0700

    Updates

--- bundle.js
+++ bundle.js
@@ -32,7 +32,6 @@ const toggleReadMoreButtonText = toggleReadMoreButton.querySelector('.hero__more
 const toggleReadMoreButtonExpandIcon = toggleReadMoreButton.querySelector('.hero__expand-icon');
 const textSecond = document.querySelector('.hero__text_second');
 const textAdd = document.querySelector('.hero__add-text');
-const textThird = document.querySelector('.hero__text_third');
 let expanded = false;
 carouselButtons.forEach(btn => {
   btn.addEventListener('click', function () {
@@ -45,17 +44,40 @@ carouselButtons.forEach(btn => {
 });
 toggleReadMoreButton.addEventListener('click', function () {
   expanded = !expanded;
-  toggleReadMoreButtonText.textContent = expanded ? 'Скрыть' : 'Читать далее';
-  toggleReadMoreButtonExpandIcon.classList.toggle('hero__expand-icon_active');
-  if (window.innerWidth >= 320 && window.innerWidth < 768) {
-    textSecond.classList.toggle('hero__text_visible');
-    textAdd.classList.toggle('hero__add-text_visible');
-    textThird.classList.toggle('hero__text_visible');
-  } else if (window.innerWidth >= 768 && window.innerWidth < 1120) {
-    textAdd.classList.toggle('hero__add-text_visible');
-    textThird.classList.toggle('hero__text_visible');
+  if (expanded) {
+    toggleReadMoreButtonExpandIcon.classList.add('hero__expand-icon_active');
+    toggleReadMoreButtonText.textContent = 'Скрыть';
   } else {
-    textThird.classList.toggle('hero__text_visible');
+    toggleReadMoreButtonExpandIcon.classList.remove('hero__expand-icon_active');
+    toggleReadMoreButtonText.textContent = 'Читать далее';
+  }
+  if (window.innerWidth >= 320 && window.innerWidth < 768) {
+    if (expanded) {
+      textSecond.classList.add('hero__text_visible');
+      textAdd.classList.add('hero__add-text_visible');
+    } else {
+      textSecond.classList.remove('hero__text_visible');
+      textAdd.classList.remove('hero__add-text_visible');
+    }
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1440) {
+    if (expanded) {
+      textAdd.classList.add('hero__add-text_visible');
+    } else {
+      textAdd.classList.remove('hero__add-text_visible');
+    }
+  }
+});
+window.addEventListener('resize', () => {
+  if (!expanded) return;
+  if (window.innerWidth >= 320 && window.innerWidth < 768) {
+    textSecond.classList.add('hero__text_visible');
+    textAdd.classList.add('hero__add-text_visible');
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1440) {
+    textSecond.classList.remove('hero__text_visible');
+    textAdd.classList.add('hero__add-text_visible');
+  } else {
+    textSecond.classList.remove('hero__text_visible');
+    textAdd.classList.remove('hero__add-text_visible');
   }
 });
 
